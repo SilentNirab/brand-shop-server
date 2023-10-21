669339aca5b51063341478fb4b4340b8715bbcc6
Md Nirab <nabilnirab255@gmail.com>
2023-10-21 22:10:51 +0600

    use app.put api

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/car/:id', async(req, res) =>{
+    app.put('/car/:id', async(req, res) =>{
       const id = req.params.id;
       const filter  = {_id: new ObjectId(id)};
       const options = {upsert: true};
